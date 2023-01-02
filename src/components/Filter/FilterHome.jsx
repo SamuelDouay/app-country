406bc7c011192e84da4afe4d47402b438c0f9f8a
Samuel Douay <douaysamuel@gmail.com>
2023-01-02 21:58:17 +0100

    fix debut darck mode

--- src/components/Filter/FilterHome.jsx
+++ src/components/Filter/FilterHome.jsx
@@ -1,42 +1,60 @@
 import Button from "../Global/Button";
-import { ReactComponent as SearchLigth } from "../../assets/images/loupe_white.svg"
-import { ReactComponent as SearchDarck } from "../../assets/images/loupe_black.svg"
+import { ReactComponent as SearchLigth } from "../../assets/images/loupe_white.svg";
+import { ReactComponent as SearchDarck } from "../../assets/images/loupe_black.svg";
 import { useEffect, useState } from "react";
 
-const FilterHome = ({filterRegionFunction, filterSearchFunction}) => {
-    const [darckMode, setDarckMode] = useState(false);
+const FilterHome = ({ filterRegionFunction, filterSearchFunction }) => {
+  const [darckMode, setDarckMode] = useState(false);
+  useEffect(() => {
+    setDarckMode(localStorage.getItem("darkMode").includes("Light"));
+  }, []);
 
-    useEffect(() => {
-        setDarckMode(localStorage.getItem('darkMode').includes('Ligth'))
-    }, []);
+  function handleChangeRegion(event) {
+    filterRegionFunction(event.target.value);
+  }
 
-    function handleChangeRegion(event) {
-        filterRegionFunction(event.target.value);
-    }
+  function handleChangeSearch(recherche) {
+    filterSearchFunction(recherche);
+  }
 
-    function handleChangeSearch(recherche) {
-        filterSearchFunction(recherche);
-    }
-
-    return (
-        <div id="header_componant">
-            <div id="searchContainer">
-                {darckMode?<SearchDarck/>:<SearchLigth/>}
-                <Button type='text' placeholder={'Search for a country'} filterSearchValue={handleChangeSearch}/>
-            </div>
-            <div id="filterCountry">
-                <select name="country" id="country_select" onChange={handleChangeRegion}>
-                    <option className="region_filter" value="all">All continents</option>
-                    <option className="region_filter" value="Africa">Africa</option>
-                    <option className="region_filter" value="Americas">America</option>
-                    <option className="region_filter" value="Asia">Asia</option>
-                    <option className="region_filter" value="Europe">Europa</option>
-                    <option className="region_filter" value="Oceania">Oceania</option>
-                </select>
-            </div>
-        </div>
-    );
-
+  return (
+    <div id="header_componant">
+      <div id="searchContainer">
+        {darckMode ? <SearchDarck /> : <SearchLigth />}
+        <Button
+          type="text"
+          placeholder={"Search for a country"}
+          filterSearchValue={handleChangeSearch}
+        />
+      </div>
+      <div id="filterCountry">
+        <select
+          name="country"
+          id="country_select"
+          onChange={handleChangeRegion}
+        >
+          <option className="region_filter" value="all">
+            All continents
+          </option>
+          <option className="region_filter" value="Africa">
+            Africa
+          </option>
+          <option className="region_filter" value="Americas">
+            America
+          </option>
+          <option className="region_filter" value="Asia">
+            Asia
+          </option>
+          <option className="region_filter" value="Europe">
+            Europa
+          </option>
+          <option className="region_filter" value="Oceania">
+            Oceania
+          </option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default FilterHome;
